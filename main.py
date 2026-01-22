f9193791878682b9e8497710ad2186081a8dea45
Darmi555 <mateusz.rudnicki05@wp.pl>
2026-01-22 13:22:40 +0000

    siemav2'

--- main.py
+++ main.py
@@ -8,32 +8,34 @@ def main() -> None:
         players = json.load(f)
 
     for player in players:
-        race_data = player['race']
+        race_data = player.get('race')
+        if not race_data:
+            continue
         race_obj, created = Race.objects.get_or_create(
-            name=race_data['name'],
+            name=race_data.get('name'),
             defaults={'description': race_data.get('description', '')}
         )
 
         for skill in race_data.get('skills', []):
             Skill.objects.get_or_create(
-                name=skill['name'],
+                name=skill.get('name'),
                 race=race_obj,
-                defaults={'bonus': skill['bonus']}
+                defaults={'bonus': skill.get('bonus')}
             )
 
         guild_obj = None
         guild_data = player.get('guild')
         if guild_data:
             guild_obj, created = Guild.objects.get_or_create(
-                name=guild_data['name'],
+                name=guild_data.get('name'),
                 defaults={'description': guild_data.get('description')}
             )
 
         Player.objects.get_or_create(
-            nickname=player['nickname'],
+            nickname=player.get('nickname'),
             defaults={
-            'email': player['email'],
-            'bio': player['bio'],
+            'email': player.get('email'),
+            'bio': player.get('bio'),
             'race': race_obj,
             'guild': guild_obj
             }
